08b2ffe6cf7e0d73a81f5872b3f5757954c2cdb6
Spenser <spenser@minddnd.com>
2021-08-05 23:33:46 -0400

    assets and first page

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '$lib/assets/global.scss'
+</script>
+
+<div class="app">
+	<slot />
+</div>
+
+<style>
+	.app{
+		min-height: 100vh;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="home split-screen">
+    <div class="color-half">
+      <div class="container">
+        <p class="page-title">Heat Capacity</p>
+
+        <div class="content">
+          <h1>Specific Heat Capacity</h1>
+
+          <p><strong>You are an engineer</strong> preparing to build a new home. You want to use material that will keep the house well insulated so it will be more energy efficient. <strong>One factor affecting your decision is the heat capacity of the materials.</strong></p>
+
+
+          <div class="btn-row">
+            <a href="/quiz/materials" class="btn hollow">Skip To Quiz</a>
+            <a href="/review" class="btn">Learn More&nbsp;&rarr;</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+        <video class="pic-half" autoplay muted loop>
+          <source src="./science.mp4" type="video/mp4">
+          Your browser does not support the video tag.
+        </video> 
+  </div>
+
+	<style>
+		.home{
+			min-height: 100vh;
+		}
+	</style>
